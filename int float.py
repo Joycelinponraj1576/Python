ff390e4122afaef9f250d6e387e61fed792cd0d6
Joycelinponraj1576 <joycelinponraj5807@gmail.com>
2026-06-19 09:38:47 +0530

    Add files via upload

---
+++ int float.py
@@ -0,0 +1,13 @@
+a=input("Enter the first value")
+b=input("Enter the second value")
+print(a+b)
+
+
+a=int (input("Enter the first value"))
+b=int (input("Enter the second value"))
+print(a+b) 
+
+
+a=float (input("Enter the first value"))
+b=float (input("Enter the second value"))
+print(a+b) 
