c21d25b2a0064328d40732258a06370d915c252b
Joycelin Ponraj <joycelinponraj5807@gmail.com>
2026-07-16 09:37:00 +0530

    Add files via upload

---
+++ String Method.py
@@ -0,0 +1,129 @@
+word = 'banana'
+new_word = word.upper()
+print(new_word)
+
+new_word = word.lower()
+print(new_word)
+
+new_word = word.capitalize()
+print(new_word)
+
+new_word = word.casefold()
+print(new_word)
+
+new_word = word.encode()
+print(new_word)
+
+new_word = word.endswith('d')
+print(new_word)
+
+new_word = word.expandtabs()
+print(new_word)
+
+new_word = word.find()
+print(new_word)
+
+new_word = word.format()
+print(new_word)
+
+new_word = word.format_map()
+print(new_word)
+
+new_word = word.index()
+print(new_word)
+
+new_word = word.isalnum()
+print(new_word)
+
+new_word = word.isalpha()
+print(new_word)
+
+new_word = word.isdecimal()
+print(new_word)
+
+new_word = word.isdigit()
+print(new_word)
+
+new_word = word.isidentifier()
+print(new_word)
+
+new_word = word.islower()
+print(new_word)
+
+new_word = word.isnumeric()
+print(new_word)
+
+new_word = word.isprintable()
+print(new_word)
+
+new_word = word.isspace()
+print(new_word)
+
+new_word = word.istitle()
+print(new_word)
+
+new_word = word.isupper()
+print(new_word)
+
+new_word = word.join()
+print(new_word)
+
+new_word = word.ljust()
+print(new_word)
+
+new_word = word.lower()
+print(new_word)
+
+new_word = word.lstrip()
+print(new_word)
+
+new_word = word.maketrans()
+print(new_word)
+
+new_word = word.partition()
+print(new_word)
+
+new_word = word.replace()
+print(new_word)
+
+new_word = word.rfind()
+print(new_word)
+
+new_word = word.rindex()
+print(new_word)
+
+new_word = word.rjust()
+print(new_word)
+
+new_word = word.rsplit()
+print(new_word)
+
+new_word = word.rstrip()
+print(new_word)
+
+new_word = word.split()
+print(new_word)
+
+new_word = word.splitlines()
+print(new_word)
+
+new_word = word.startswith()
+print(new_word)
+
+new_word = word.strip()
+print(new_word)
+
+new_word = word.swapcase()
+print(new_word)
+
+new_word = word.title()
+print(new_word)
+
+new_word = word.translate()
+print(new_word)
+
+new_word = word.upper()
+print(new_word)
+
+new_word = word.zfill()
+print(new_word)
